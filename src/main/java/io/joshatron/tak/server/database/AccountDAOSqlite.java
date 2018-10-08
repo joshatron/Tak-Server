cfbd61b073423475cd229fc81a2649812e069911
Joshua Leger <legermjoshua@gmail.com>
2018-10-07 17:26:11 -0700

    added sql closing for accountdao

--- src/main/java/io/joshatron/tak/server/database/AccountDAOSqlite.java
+++ src/main/java/io/joshatron/tak/server/database/AccountDAOSqlite.java
@@ -23,18 +23,31 @@ public class AccountDAOSqlite implements AccountDAO {
 
     @Override
     public boolean isAuthenticated(Auth auth) throws SQLException {
+        PreparedStatement stmt = null;
+        ResultSet rs = null;
+
         String getAuth = "SELECT username, auth " +
                 "FROM users " +
                 "WHERE username = ?;";
 
-        PreparedStatement stmt = conn.prepareStatement(getAuth);
-        stmt.setString(1, auth.getUsername());
-        ResultSet rs = stmt.executeQuery();
+        try {
+            stmt = conn.prepareStatement(getAuth);
+            stmt.setString(1, auth.getUsername());
+            rs = stmt.executeQuery();
 
-        if(rs.next()) {
-            if(auth.getUsername().equals(rs.getString("username")) &&
-                BCrypt.checkpw(auth.getPassword(), rs.getString("auth"))) {
-                return true;
+            if (rs.next()) {
+                if (auth.getUsername().equals(rs.getString("username")) &&
+                        BCrypt.checkpw(auth.getPassword(), rs.getString("auth"))) {
+                    return true;
+                }
+            }
+        }
+        finally {
+            if(stmt != null) {
+                stmt.close();
+            }
+            if(rs != null) {
+                rs.close();
             }
         }
 
@@ -43,27 +56,44 @@ public class AccountDAOSqlite implements AccountDAO {
 
     @Override
     public boolean registerUser(Auth auth) throws SQLException {
+        PreparedStatement selectStmt = null;
+        PreparedStatement stmt = null;
+        ResultSet rs = null;
+
         //make sure username isn't taken
         String checkUsername = "SELECT username " +
                 "FROM users " +
                 "WHERE username = ?;";
 
-        PreparedStatement selectStmt = conn.prepareStatement(checkUsername);
-        selectStmt.setString(1, auth.getUsername());
-        ResultSet rs = selectStmt.executeQuery();
-
-        if(rs.next()) {
-            return false;
-        }
-
         //insert new user if it isn't
         String insertUser = "INSERT INTO  users (username, auth) " +
                 "VALUES (?,?)";
 
-        PreparedStatement stmt = conn.prepareStatement(insertUser);
-        stmt.setString(1, auth.getUsername());
-        stmt.setString(2, BCrypt.hashpw(auth.getPassword(), BCrypt.gensalt()));
-        stmt.executeUpdate();
+        try {
+            selectStmt = conn.prepareStatement(checkUsername);
+            selectStmt.setString(1, auth.getUsername());
+            rs = selectStmt.executeQuery();
+
+            if (rs.next()) {
+                return false;
+            }
+
+            stmt = conn.prepareStatement(insertUser);
+            stmt.setString(1, auth.getUsername());
+            stmt.setString(2, BCrypt.hashpw(auth.getPassword(), BCrypt.gensalt()));
+            stmt.executeUpdate();
+        }
+        finally {
+            if(selectStmt != null) {
+                selectStmt.close();
+            }
+            if(stmt != null) {
+                stmt.close();
+            }
+            if(rs != null) {
+                rs.close();
+            }
+        }
 
         return true;
     }
@@ -77,45 +107,72 @@ public class AccountDAOSqlite implements AccountDAO {
             return false;
         }
 
+        PreparedStatement stmt = null;
+
         String changePass = "UPDATE users " +
                 "SET auth = ? " +
                 "WHERE username = ?;";
 
-        PreparedStatement stmt = conn.prepareStatement(changePass);
-        stmt.setString(1, BCrypt.hashpw(change.getUpdated(), BCrypt.gensalt()));
-        stmt.setString(2, change.getAuth().getUsername());
-        stmt.executeUpdate();
+        try {
+            stmt = conn.prepareStatement(changePass);
+            stmt.setString(1, BCrypt.hashpw(change.getUpdated(), BCrypt.gensalt()));
+            stmt.setString(2, change.getAuth().getUsername());
+            stmt.executeUpdate();
+        }
+        finally {
+            if(stmt != null) {
+                stmt.close();
+            }
+        }
 
         return true;
     }
 
     public int idFromUsername(String username) throws SQLException {
+        PreparedStatement selectStmt = null;
+
         String checkUsername = "SELECT id " +
                 "FROM users " +
                 "WHERE username = ?;";
 
-        PreparedStatement selectStmt = conn.prepareStatement(checkUsername);
-        selectStmt.setString(1, username);
-        ResultSet rs = selectStmt.executeQuery();
+        try {
+            selectStmt = conn.prepareStatement(checkUsername);
+            selectStmt.setString(1, username);
+            ResultSet rs = selectStmt.executeQuery();
 
-        if(rs.next()) {
-            return rs.getInt("id");
+            if (rs.next()) {
+                return rs.getInt("id");
+            }
+        }
+        finally {
+            if(selectStmt != null) {
+                selectStmt.close();
+            }
         }
 
         return -9999;
     }
 
     public String usernameFromId(int id) throws SQLException {
+        PreparedStatement selectStmt = null;
+
         String checkUsername = "SELECT username " +
                 "FROM users " +
                 "WHERE id = ?;";
 
-        PreparedStatement selectStmt = conn.prepareStatement(checkUsername);
-        selectStmt.setInt(1, id);
-        ResultSet rs = selectStmt.executeQuery();
+        try {
+            selectStmt = conn.prepareStatement(checkUsername);
+            selectStmt.setInt(1, id);
+            ResultSet rs = selectStmt.executeQuery();
 
-        if(rs.next()) {
-            return rs.getString("username");
+            if (rs.next()) {
+                return rs.getString("username");
+            }
+        }
+        finally {
+            if(selectStmt != null) {
+                selectStmt.close();
+            }
         }
 
         return null;
