021cfec69acf39ccec87c07a6b760f54c0cb7a53
Joshua Leger <legermjoshua@gmail.com>
2019-02-25 21:04:55 -0800

    Added admin dao methods

--- src/main/java/io/joshatron/tak/server/database/AdminDAO.java
+++ src/main/java/io/joshatron/tak/server/database/AdminDAO.java
@@ -1,4 +1,15 @@
 package io.joshatron.tak.server.database;
 
+import io.joshatron.tak.server.exceptions.GameServerException;
+import io.joshatron.tak.server.request.Auth;
+import io.joshatron.tak.server.response.User;
+
 public interface AdminDAO {
+
+    boolean isAuthenticated(Auth auth) throws GameServerException;
+    void updatePassword(String newPass) throws GameServerException;
+    void banUser(String userId) throws GameServerException;
+    void unbanUser(String userId) throws GameServerException;
+    User[] searchBannedUsers(String usernameSearch) throws GameServerException;
+    User[] getBannedUsers() throws GameServerException;
 }
--- src/main/java/io/joshatron/tak/server/database/AdminDAOSqlite.java
+++ src/main/java/io/joshatron/tak/server/database/AdminDAOSqlite.java
@@ -1,5 +1,9 @@
 package io.joshatron.tak.server.database;
 
+import io.joshatron.tak.server.exceptions.GameServerException;
+import io.joshatron.tak.server.request.Auth;
+import io.joshatron.tak.server.response.User;
+
 import java.sql.Connection;
 
 public class AdminDAOSqlite implements AdminDAO {
@@ -8,5 +12,35 @@ public class AdminDAOSqlite implements AdminDAO {
 
     public AdminDAOSqlite(Connection conn) {
         this.conn = conn;
+    }
+
+    @Override
+    public boolean isAuthenticated(Auth auth) throws GameServerException {
+        return false;
+    }
+
+    @Override
+    public void updatePassword(String newPass) throws GameServerException {
+
+    }
+
+    @Override
+    public void banUser(String userId) throws GameServerException {
+
+    }
+
+    @Override
+    public void unbanUser(String userId) throws GameServerException {
+
+    }
+
+    @Override
+    public User[] searchBannedUsers(String usernameSearch) throws GameServerException {
+        return new User[0];
+    }
+
+    @Override
+    public User[] getBannedUsers() throws GameServerException {
+        return new User[0];
     }
 }
