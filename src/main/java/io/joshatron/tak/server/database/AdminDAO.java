5ac1c4ac70ea04d01376d584e51e8acc019abbee
Joshua Leger <legermjoshua@gmail.com>
2019-02-27 20:57:13 -0800

    Finished v1 of admin dao

--- src/main/java/io/joshatron/tak/server/database/AdminDAO.java
+++ src/main/java/io/joshatron/tak/server/database/AdminDAO.java
@@ -2,7 +2,6 @@ package io.joshatron.tak.server.database;
 
 import io.joshatron.tak.server.exceptions.GameServerException;
 import io.joshatron.tak.server.request.Auth;
-import io.joshatron.tak.server.response.User;
 
 public interface AdminDAO {
 
@@ -11,6 +10,4 @@ public interface AdminDAO {
     void updatePassword(String newPass) throws GameServerException;
     void banUser(String userId) throws GameServerException;
     void unbanUser(String userId) throws GameServerException;
-    User[] searchBannedUsers(String usernameSearch) throws GameServerException;
-    User[] getBannedUsers() throws GameServerException;
 }
